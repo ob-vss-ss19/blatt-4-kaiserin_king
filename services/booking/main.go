e60a71f38f2f0f00616c4ace49aeeef8c22d714e
wildeKaiserin <friedri5@hm.edu>
2019-06-04 12:05:09 +0200

    first finish booking

--- services/booking/main.go
+++ services/booking/main.go
@@ -26,11 +26,36 @@ func (bs *BService) CreateBooking(ctx context.Context, req *booking.CreateBookin
 
 func (bs *BService) DeleteBooking(ctx context.Context, req *booking.DeleteBookingRequest, rsp *booking.DeleteBookingResult) error {
 	// Delete from booking or notConfirmed list
+	for i, b := range bs.booking {
+		if b.Id == req.Id {
+			bs.booking = append(bs.booking[:i], bs.booking[i+1:]...)
+			rsp.Successful = true
+			return nil
+		}
+	}
+	for i, b := range bs.notConfirmed {
+		if b.Id == req.Id {
+			bs.notConfirmed = append(bs.notConfirmed[:i], bs.notConfirmed[i+1:]...)
+			rsp.Successful = true
+			return nil
+		}
+	}
+	rsp.Successful = false
 	return nil
 }
 
 func (bs *BService) ConfirmBooking(ctx context.Context, req *booking.ConfirmBookingRequest, rsp *booking.ConfirmBookingResult) error {
 	// move booking from notConfirmed to booking list
+	for i, b := range bs.notConfirmed {
+		if b.Id == req.Id {
+			bs.booking = append(bs.booking, b)
+			// aus notConfirmed loeschen
+			bs.notConfirmed = append(bs.notConfirmed[:i], bs.notConfirmed[i+1:]...)
+			rsp.Successful = true
+			return nil
+		}
+	}
+	rsp.Successful = false
 	return nil
 }
 
