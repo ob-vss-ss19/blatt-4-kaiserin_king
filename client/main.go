bd0f8030b88efc8891abf73369083c3c763607e3
maxiking203 <Maximilian.Sachmann@gmx.de>
2019-06-10 12:56:23 +0200

    consul

--- client/main.go
+++ client/main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/micro/go-micro"
+	booking "github.com/ob-vss-ss19/blatt-4-kaiserin_king/services/booking/proto"
+	cinema "github.com/ob-vss-ss19/blatt-4-kaiserin_king/services/cinema/proto"
+	movie "github.com/ob-vss-ss19/blatt-4-kaiserin_king/services/movie/proto"
+	show "github.com/ob-vss-ss19/blatt-4-kaiserin_king/services/show/proto"
 	user "github.com/ob-vss-ss19/blatt-4-kaiserin_king/services/user/proto"
 )
 
@@ -27,12 +31,12 @@ func main() {
 	fmt.Println("test")*/
 
 	//var client client.Client
-/*	showC := show.NewShowService("go.micro.services.show", service.Client())
+	showC := show.NewShowService("go.micro.services.show", service.Client())
 	bookingC := booking.NewBookingService("go.micro.services.booking", service.Client())
 	cinemaC := cinema.NewCinemaService("go.micro.services.cinema", service.Client())
-	movieC := movie.NewMovieService("go.micro.services.movie", service.Client())*/
+	movieC := movie.NewMovieService("go.micro.services.movie", service.Client())
 	userC := user.NewUserService("go.micro.services.user", service.Client())
-/*
+
 	rspS , err := showC.GetShowList(context.TODO(), &show.GetShowListRequest{})
 	if err != nil {
 		fmt.Println(err)
@@ -61,7 +65,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("List of all movies: %v \n\n", rspM.Movies)*/
+	fmt.Printf("List of all movies: %v \n\n", rspM.Movies)
 
 	rspU, err := userC.GetUserList(context.TODO(), &user.GetUserListRequest{})
 	if err != nil {
@@ -69,15 +73,56 @@ func main() {
 	}
 	fmt.Printf("List of all users: %v \n\n", rspU.Users)
 
-	rspCU, err := userC.CreateUser(context.TODO(), &user.CreateUserRequest{Name: "Max Mustermann"})
+	fmt.Println("##########")
+	fmt.Println("##########")
+	fmt.Println("")
+
+	rspCD, err := cinemaC.DeleteHall(context.TODO(), &cinema.DeleteHallRequest{Id:2})
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("List of all users: %v \n\n", rspCU.Id)
+	fmt.Printf("List of all halls: %v \n\n", rspCD.Successful)
+
+	fmt.Println("##########")
+	fmt.Println("##########")
+	fmt.Println("")
+
+	rspS , err = showC.GetShowList(context.TODO(), &show.GetShowListRequest{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("List of all shows: %v \n\n", rspS.Shows)
+
+	rspB, err = bookingC.GetBookingList(context.TODO(), &booking.GetListRequest{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("List of all Bookings: %v \n\n", rspB.Bookings)
+
+	rspBNC, err = bookingC.GetNotConfirmedList(context.TODO(), &booking.GetListRequest{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("List of all not confirmed Bookings: %v \n\n", rspBNC.Bookings)
+
+	rspC, err = cinemaC.GetHallList(context.TODO(), &cinema.GetHallListRequest{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("List of all halls: %v \n\n", rspC.CHall)
+
+	rspM, err = movieC.GetMovieList(context.TODO(), &movie.GetMovieListRequest{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("List of all movies: %v \n\n", rspM.Movies)
 
 	rspU, err = userC.GetUserList(context.TODO(), &user.GetUserListRequest{})
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Printf("List of all users: %v \n\n", rspU.Users)
+
+
+
 }
--- services/user/main.go
+++ services/user/main.go
@@ -119,7 +119,7 @@ func main() {
 	)
 
 	service.Init()
-	user.RegisterUserHandler(service.Server(), &UService{user: exampleData(), nextID: 5})
+	_ = user.RegisterUserHandler(service.Server(), &UService{user: exampleData(), nextID: 5})
 	r := service.Run()
 	if r != nil {
 		log.Fatalf("Running service failed! %v\n", r.Error())
@@ -129,16 +129,16 @@ func main() {
 func exampleData() []*user.UserData {
 	users := make([]*user.UserData, 0)
 	users = append(users, &user.UserData{Id: 1, Name: "Maxi King",
-		Bookings: make([]int32, 0), NotConfirmed: make([]int32, 0)})
+		Bookings:[]int32{3}, NotConfirmed: make([]int32, 0)})
 
-	users = append(users, &user.UserData{Id: 2, Name: "Kasierin Sissy",
-		Bookings: make([]int32, 0), NotConfirmed: make([]int32, 0)})
+	users = append(users, &user.UserData{Id: 2, Name: "Kaiserin Sissy",
+		Bookings: []int32{4}, NotConfirmed: make([]int32, 0)})
 
 	users = append(users, &user.UserData{Id: 3, Name: "Simon der Weise",
-		Bookings: make([]int32, 0), NotConfirmed: make([]int32, 0)})
+		Bookings: []int32{1}, NotConfirmed: make([]int32, 0)})
 
 	users = append(users, &user.UserData{Id: 4, Name: "Lisa Master",
-		Bookings: make([]int32, 0), NotConfirmed: make([]int32, 0)})
+		Bookings: []int32{2}, NotConfirmed: make([]int32, 0)})
 
 	return users
 }
